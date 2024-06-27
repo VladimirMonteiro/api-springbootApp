0eaedaa7f1cfdf99d760b08de77f3ff9ca41e9a8
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2024-06-27 18:48:27 -0300

    user class resource findById

---
+++ src/main/java/com/example/api/resources/UserResources.java
@@ -0,0 +1,19 @@
+package com.example.api.resources;
+
+import com.example.api.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+
+
+    }
+}
