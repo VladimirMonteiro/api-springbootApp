debd1009b6cab587df94ce787b6f5f259fc55052
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2024-06-27 18:40:34 -0300

    user class created

---
+++ src/main/java/com/example/api/domain/User.java
@@ -0,0 +1,24 @@
+package com.example.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "TB_USERS")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
