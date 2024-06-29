b84329073e5902686f1d10c7c190ffdff76cb343
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2024-06-29 20:26:15 -0300

    test ApiApllication

--- src/test/java/com/example/api/ApiApplicationTests.java
+++ src/test/java/com/example/api/ApiApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		ApiApplication.main(new String[] {});
 	}
 
 }
